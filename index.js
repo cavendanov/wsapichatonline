31a9ec7e1dd15913cc418b00c8c610c50e89aacf
cavendanov <carlosavendaov@gmail.com>
2025-10-24 15:05:20 -0500

    ajustes al get y al post

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => res.send("Hello word"));
+// Set port and verify_token
+const port = process.env.PORT || 3000;
+const verifyToken = "04112b51bde79531467dc65f09e9231a";
 
-app.listen(3000);
-console.log("Server on port 3000");
+app.get('/', (req, res) => {
+  const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
+
+  if (mode === 'subscribe' && token === verifyToken) {
+    console.log('WEBHOOK VERIFIED');
+    res.status(200).send(challenge);
+  } else {
+    res.status(403).end();
+  }
+});
+
+// Route for POST requests
+app.post('/', (req, res) => {
+  const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
+  console.log(`\n\nWebhook received ${timestamp}\n`);
+  console.log(JSON.stringify(req.body, null, 2));
+  res.status(200).end();
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`\nListening on port ${port}\n`);
+});
